42821fadb8ed42bf7cc53cf2157e85200c645d53
Max Fellows <max.fellows@nrcan.gc.ca>
2024-12-20 10:05:15 -0800

    Fixed Recliner2GCBM compatibility.

--- gcbminputloader/project/projectfactory.py
+++ gcbminputloader/project/projectfactory.py
@@ -93,7 +93,7 @@ class ProjectFactory:
         if recliner2gcbm_project_type is not None:
             return (
                 ProjectType.LegacyGcbmClassicSpatialNoGrowthCurves if recliner2gcbm_project_type == 1
-                else ProjectType.GcbmClassicSpatial if config.get("aidb").suffix == ".db"
+                else ProjectType.GcbmClassicSpatial if config.get("AIDBPath", "").endswith(".db")
                 else ProjectType.LegacyGcbmClassicSpatial
             )
             
