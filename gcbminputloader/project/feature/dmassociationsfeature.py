1dffe940d3b0706d632a836f05011c1761e713eb
Max Fellows <max.fellows@nrcan.gc.ca>
2026-02-11 14:39:59 -0800

    Added missing Access AIDB support to DM associations loader.

--- gcbminputloader/project/feature/dmassociationsfeature.py
+++ gcbminputloader/project/feature/dmassociationsfeature.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 import logging
+import gcbminputloader
+from pathlib import Path
 from collections import defaultdict
 from sqlalchemy import text
 from gcbminputloader.project.feature.feature import Feature
 from gcbminputloader.util.db import get_connection
+from gcbminputloader.util.json import InputLoaderJson
 
 class DMAssociationsFeature(Feature):
     
@@ -19,39 +22,11 @@ class DMAssociationsFeature(Feature):
             spu_lookup = self._get_spu_lookup(output_db)
             dist_type_lookup = self._get_dist_type_lookup(output_db)
             dm_lookup = self._get_dm_lookup(output_db)
-
-            aidb_dms = aidb.execute(text(
-                """
-                SELECT
-                    a_tr.name AS admin,
-                    e_tr.name AS eco,
-                    dt_tr.name AS dist_type,
-                    dm.id || '_' || dm_tr.name AS dm
-                FROM disturbance_matrix_association dma
-                INNER JOIN spatial_unit spu
-                    ON dma.spatial_unit_id = spu.id
-                INNER JOIN eco_boundary e
-                    ON spu.eco_boundary_id = e.id
-                INNER JOIN eco_boundary_tr e_tr
-                    ON e.id = e_tr.eco_boundary_id
-                INNER JOIN admin_boundary a
-                    ON spu.admin_boundary_id = a.id
-                INNER JOIN admin_boundary_tr a_tr
-                    ON a.id = a_tr.admin_boundary_id
-                INNER JOIN disturbance_type dt
-                    ON dma.disturbance_type_id = dt.id
-                INNER JOIN disturbance_type_tr dt_tr
-                    ON dt.id = dt_tr.disturbance_type_id
-                INNER JOIN disturbance_matrix dm
-                    ON dma.disturbance_matrix_id = dm.id
-                INNER JOIN disturbance_matrix_tr dm_tr
-                    ON dm.id = dm_tr.disturbance_matrix_id
-                WHERE e_tr.locale_id = 1
-                    AND a_tr.locale_id = 1
-                    AND dt_tr.locale_id = 1
-                    AND dm_tr.locale_id = 1
-                """
-            ))
+            aidb_dms = (
+                self._get_aidb_dm_associations(aidb)
+                if Path(self._aidb_path).suffix in (".accdb", ".mdb")
+                else self._get_cbm_defaults_dm_associations(aidb)
+            )
 
             output_db.execute(
                 text(
@@ -68,6 +43,47 @@ class DMAssociationsFeature(Feature):
                     } for row in aidb_dms
                 ]
             )
+
+    def _get_cbm_defaults_dm_associations(self, conn: Connection) -> CursorResult:
+        return conn.execute(text(
+            """
+            SELECT
+                a_tr.name AS admin,
+                e_tr.name AS eco,
+                dt_tr.name AS dist_type,
+                dm.id || '_' || dm_tr.name AS dm
+            FROM disturbance_matrix_association dma
+            INNER JOIN spatial_unit spu
+                ON dma.spatial_unit_id = spu.id
+            INNER JOIN eco_boundary e
+                ON spu.eco_boundary_id = e.id
+            INNER JOIN eco_boundary_tr e_tr
+                ON e.id = e_tr.eco_boundary_id
+            INNER JOIN admin_boundary a
+                ON spu.admin_boundary_id = a.id
+            INNER JOIN admin_boundary_tr a_tr
+                ON a.id = a_tr.admin_boundary_id
+            INNER JOIN disturbance_type dt
+                ON dma.disturbance_type_id = dt.id
+            INNER JOIN disturbance_type_tr dt_tr
+                ON dt.id = dt_tr.disturbance_type_id
+            INNER JOIN disturbance_matrix dm
+                ON dma.disturbance_matrix_id = dm.id
+            INNER JOIN disturbance_matrix_tr dm_tr
+                ON dm.id = dm_tr.disturbance_matrix_id
+            WHERE e_tr.locale_id = 1
+                AND a_tr.locale_id = 1
+                AND dt_tr.locale_id = 1
+                AND dm_tr.locale_id = 1
+            """
+        ))
+
+    def _get_aidb_dm_associations(self, conn: Connection) -> CursorResult:
+        resource_root = Path(gcbminputloader.__file__).parent.joinpath("resources", "Loader")
+        resource_path = resource_root.joinpath("Legacy", "disturbance_matrix_associations.json")
+        sql = InputLoaderJson(resource_path).load()["SQLLoaderMapping"]["fetch_sql"]
+
+        return conn.execute(text(sql))
 
     def _get_spu_lookup(self, conn: Connection) -> dict[str, dict[str, int]]:
         spu_lookup = defaultdict(dict)
